99e3bc6d0e2030f41c910837e6dc38092c1dc4ca
MichaelMMMMM <meindlmichael@web.de>
2017-02-09 16:59:02 +0100

    Dokumentation TM 1D & 3D, RT 3D

---
+++ Bachelor/8_Regelung3D/RT_3D.m
@@ -0,0 +1,79 @@
+%30.01.2017, Michael Meindl
+
+m      = 1.532;
+m_R    = 0.155;
+r_1    = [1;7.5;7.5]*1e-2;
+r_2    = [7.5;1;7.5]*1e-2;
+r_3    = [7.5;7.5;1]*1e-2;
+l_C    = 0.066;
+c      = [l_C;l_C;l_C]; 
+C_psi  = 3.1176e-5;
+
+I_R1M1 = [3.358e-4, 0, 0; 0, 1.961e-4, 0; 0, 0, 1.681e-4];
+I_R2M2 = [1.691e-4, 0, 0; 0, 3.358e-4, 0; 0, 0, 1.961e-4];
+I_R3M3 = [1.961e-4, 0, 0; 0, 1.691e-4, 0; 0, 0, 3.358e-4];
+
+I_GHO  = [1.520e-2, -5.201e-3, 5.375e-3; 
+          -5.201e-3, 1.52e-2, 5.225e-3;
+          5.375e-3, 5.225e-3, 1.542e-2];
+I_KO   = I_GHO + m_R*(eye(3)*dot(r_1,r_1)-r_1*r_1') ...
+               + m_R*(eye(3)*dot(r_2,r_2)-r_2*r_2') ...
+               + m_R*(eye(3)*dot(r_3,r_3)-r_3*r_3');
+           
+I_K    = I_KO+[I_R2M2(1,1)+I_R3M3(1,1), 0, 0;
+               0, I_R1M1(2,2)+I_R3M3(2,2), 0;
+               0, 0, I_R1M1(3,3)+I_R2M2(1,1)];
+I_R    = [I_R1M1(1,1), 0, 0; 0, I_R2M2(2,2), 0; 0, 0, I_R3M3(3,3)];
+
+I_K_inv = inv(I_K);
+I_R_inv = inv(I_R);
+           
+phi_10 = 0;
+phi_20 = -2*atan(sqrt(2)-sqrt(3));
+phi_30 = -pi/4;
+
+g      = 9.81;
+g_LA   = [0, g*sin(phi_20), g*cos(phi_20)*sin(phi_30);
+          -g*sin(phi_20), 0, g*cos(phi_20)*cos(phi_30);
+          -g*cos(phi_20)*sin(phi_30), -g*cos(phi_20)*cos(phi_30), 0];
+G_LA   = [0, -m*l_C, m*l_C; m*l_C, 0, -m*l_C; -m*l_C, m*l_C, 0];
+
+A = [zeros(3,3), g_LA, zeros(3,3); 
+     I_K_inv*G_LA, -C_psi*I_K_inv, C_psi*I_K_inv;
+     zeros(3,3), C_psi*I_R_inv, -C_psi*I_R_inv];
+B = [zeros(3,3); -I_K_inv; I_R_inv];
+
+C = [eye(3), zeros(3,6)];
+D = zeros(3,3);
+
+mySS = ss(A, B, C, D);
+
+g_max   = 0.5;
+u_K_max = degtorad(50);
+u_R_max = degtorad(360/60*300);
+T_max   = 0.110;
+
+
+[reduced_SS, VK] = minreal(mySS);
+
+x_max = [g_max; g_max; g_max; u_K_max; u_K_max; u_K_max; u_R_max; u_R_max; u_R_max];
+x__max = VK*x_max;
+
+Q    = diag((x__max(1:7)).^(-2), 0);
+R    = eye(3)*T_max^(-2);
+
+E = eig(A);
+
+ctrb_ranks = zeros(size(E));
+
+x_0 = ones(9,1)*0;
+x_0(4) = 1;
+
+for k = 1:length(E)
+    ctrb_ranks(k) = rank([E(k)*eye(9) - A, B]); 
+end
+
+F = lqr(reduced_SS.A, reduced_SS.B, Q, R);
+
+F_vorMat = inv(VK);
+F_vorMat = F_vorMat(1:7, 1:9);
