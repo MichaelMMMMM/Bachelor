3c1cf3e182776e18b627c707e66f32e1eb20fc45
MichaelMMMMM <meindlmichael@web.de>
2017-02-26 22:03:05 +0100

    Berechnung 1D nichtlinear

--- Bachelor/9_NL_Regelung/NL_1D_Model.m
+++ Bachelor/9_NL_Regelung/NL_1D_Model.m
@@ -1,12 +1,14 @@
 %25.02.2017, Michael Meindl, Berechnung und Analyse des nichtlinearen
 %Modells zur Beschreibung der Wuerfelseite
 
+close all;
+
 l_M   = 0.084;
 l_AC  = 0.087;
 m_r   = 0.09;
 m_k   = 0.221;
 m   = m_r + m_k;
-l_C   = (l_M*m_r+l_AC*m_k)/m_g;
+l_C   = (l_M*m_r+l_AC*m_k)/m;
 I_KO  = 2.8e-3;
 I_RM  = 1.1683e-4;
 I_GO  = I_KO+I_RM+m_r*l_M^2;
@@ -15,6 +17,16 @@ g     = 9.81;
 
 k = [-0.9120, -0.0773, -0.0001];
 
-x_0 = degtorad([3;0;0]);
+u_max = 0.035;
+phi0 = degtorad(-10:1:10);
+n    = length(phi0);
 
-u_max = 0.07;
+stop_time = 1;
+
+x_offset = zeros(3,1);
+x_noise  = zeros(3,1);
+
+NL_1D_Model_u_unlimited;
+NL_1D_Model_u_limited;
+NL_1D_Model_u_limited_offset;
+NL_1D_Model_u_limited_noise;
---
+++ Bachelor/9_NL_Regelung/NL_1D_Model_u_limited.m
@@ -0,0 +1,39 @@
+%26.02.2017, Michael Meindl, Berechnung des geschlossenen RK bei
+%unbegrenztem Drehmoment
+
+limit_on = true;
+t   = cell(1,n);
+tu  = cell(1,n);
+x1  = cell(1,n);
+x3  = cell(1,n);
+u   = cell(1,n);
+
+
+for r = 1:n
+    x_0 = [phi0(r); 0; 0];
+    sim('closedloop_1D.slx');
+    
+    t{r}   = x__SIM.Time;
+    x1{r}  = x__SIM.Data(:,1);
+    x3{r}  = x__SIM.Data(:,3);
+    u{r}   = u__SIM.Data(:,2);
+    tu{r}  = u__SIM.Time;
+end
+
+figure;
+plot(cell2mat(t), radtodeg(cell2mat(x1)), 'black'); grid;
+title('Verlauf x1, limitierte Stellgröße');
+xlabel('Zeit in Sekunden', 'interpreter', 'latex');
+ylabel('$\varphi$', 'interpreter', 'latex');
+
+figure;
+plot(cell2mat(t), cell2mat(x3), 'black'); grid;
+title('Verlauf x3, limitierte Stellgröße');
+xlabel('Zeit in Sekunden', 'interpreter', 'latex');
+ylabel('$\dot{\psi}$', 'interpreter', 'latex');
+
+figure;
+plot(cell2mat(tu), cell2mat(u), 'black'); grid;
+title('Verlauf der Stellgröße');
+xlabel('Zeit in Sekunden', 'interpreter', 'latex');
+ylabel('$u_c$', 'interpreter', 'latex');
